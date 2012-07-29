37eb649efeab9bbbf6337f96c314389f900b2e6d
Dustin Haffner <dh@xix.org>
2012-07-28 18:51:38 -0700

    add clean version of codebase

---
+++ chrome/warhol.js
@@ -0,0 +1,48 @@
+(function(local) {
+	var style = '<style id="warhol-css" type="text/css"></style>';
+
+	var url = function(path) {
+		return ['http://', local, '/', path].join('');
+	};
+
+	var js = function(hostname) {
+		$.ajax({
+			url: url(hostname + '.js'),
+			dataType: 'text',
+			success: function(source) {
+				if (!source.length)
+					return;
+
+				$(function() {
+					eval(source);
+				});
+			},
+			error: function() { }  // Fail silently.
+		});
+	};
+
+	var css = function(hostname) {
+		$.ajax({
+			url: url(hostname + '.css'),
+			dataType: 'text',
+			success: function(source) {
+				if (!document.head || !source.length)
+					return;
+
+				var element = $(style).html(source);
+				$(document.head).append(element);
+			},
+			error: function() { }  // Fail silently.
+		});
+	};
+
+	var hostname = window.location.host;
+	if (/^www\./i.test(hostname))
+		hostname = hostname.substring(4);
+
+	$(function() {
+		js(hostname);
+	});
+	
+	css(hostname);
+})('localhost:1928');
